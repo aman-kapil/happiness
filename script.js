d0ec9d4a1cc9678939fc245440db515c073bb228
Aman Kapil <amankapil058@gmail.com>
2026-04-20 15:52:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function changeColor() {
+    var img = document.getElementById("images");
+    img.style.filter = "grayscale(100%)";
+    }
+
+
+function resetColor() {
+    var img = document.getElementById("images");
+    img.style.filter = "none";
+}
